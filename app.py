8fba807b7bf3c29cb0d759bfe90b3a450576269a
senbasenba <senba.senba19@gmail.com>
2024-10-31 12:56:29 +0900

    no message

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})  # CORS設定を更新
+CORS(app, resources={r"/api/*": {"origins": "*"}})  # すべてのオリジンを許可
 
 @app.route('/', methods=['GET'])
 def hello():
